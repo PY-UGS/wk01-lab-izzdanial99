6cf9db19af7c71689c414e8ac4a9415889004392
izzdanial <41255403+izzdanial99@users.noreply.github.com>
2022-01-19 03:08:44 +0800

    Add files via upload

---
+++ Lab1_Q2a.java
@@ -0,0 +1,5 @@
+public class Lab1_Q2a {
+  public static void main(String[] args) {
+    System.out.println("Hello, I am Izz!");
+  }
+}
---
+++ Lab1_Q2c.java
@@ -0,0 +1,9 @@
+public class Lab1_Q2c{
+    public static void main(String args[]){
+
+        for(int x = 101; x > 66; x = x-2){
+            System.out.print(x);
+            System.out.print("\n");
+        }
+    }
+}
